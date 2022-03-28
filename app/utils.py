5d8a8d8c0c151601595671ab754fe2bbdcab1fc0
afv14 <97483119+afv14@users.noreply.github.com>
2022-03-28 18:16:39 -0400

    Nested main

--- app/utils.py
+++ app/utils.py
@@ -12,3 +12,19 @@ def to_usd(my_price):
     Invoke like this: to_usd(9.9999)
     """
     return '${:,.2f}'.format(my_price)
+
+
+if __name__ == "__main__":
+
+    #if this code is in the global scope of a file we are trying to import from
+    #it will throw errors when we try to run those
+    #have to nest this code in the main conditional
+
+    #nesting code in the main condition will allow other scripts to cleanly
+    #import without running this code
+
+    #this code still gets run when we invoke the script from the command line
+
+    price = input ('Please choose a price like 4.9999')
+
+    print (to_usd(float(price)))
