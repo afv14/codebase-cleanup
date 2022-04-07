ab7d1fe5a2cc9b789604b913ce08659dc426f1de
afv14 <97483119+afv14@users.noreply.github.com>
2022-04-06 20:23:08 -0400

    AtHome Game file simplified

--- app/game.py
+++ app/game.py
@@ -5,7 +5,6 @@
 from random import choice
 
 def determine_winner(user_choice, computer_choice):
-    #return "paper"
     winners = {
         "rock": {
             "rock": None,
@@ -25,7 +24,6 @@ def determine_winner(user_choice, computer_choice):
     }
     winning_choice = winners[user_choice][computer_choice]
     return winning_choice
-    #return "OOPS"
 
 
 if __name__ == "__main__":
@@ -51,30 +49,6 @@ if __name__ == "__main__":
     c = choice(valid_selections)
     print("COMPUTER CHOICE:", c)
 
-    #
-    # DETERMINATION OF WINNER
-    #
-
-    #    if u == "rock" and c == "rock":
-    #        print("It's a tie!")
-    #    elif u == "rock" and c == "paper":
-    #        print("The computer wins")
-    #    elif u == "rock" and c == "scissors":
-    #        print("The user wins")
-    #
-    #    elif u == "paper" and c == "rock":
-    #        print("The computer wins")
-    #    elif u == "paper" and c == "paper":
-    #        print("It's a tie!")
-    #    elif u == "paper" and c == "scissors":
-    #        print("The user wins")
-    #
-    #    elif u == "scissors" and c == "rock":
-    #        print("The computer wins")
-    #    elif u == "scissors" and c == "paper":
-    #        print("The user wins")
-    #    elif u == "scissors" and c == "scissors":
-    #        print("It's a tie!")
     winner = determine_winner(u,c)
     if winner == u:
         print("YOU WON")
