7f1d8c13d7bd6b76b843d092129b475fae343511
afv14 <97483119+afv14@users.noreply.github.com>
2022-04-11 17:18:50 -0400

    Merge Final

--- app/stocks.py
+++ app/stocks.py
@@ -7,15 +7,6 @@ from app.alphavantage_service import fetch_stocks_data
 
 symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
 fetch_stocks_data(symbol)
-=======
-import os
-from dotenv import load_dotenv
-from pandas import read_csv
-
-from app.utils import to_usd
-from app.alphavantage_service import fetch_stocks_data
-
-load_dotenv()
 
 symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
 fetch_stocks_data(symbol)
